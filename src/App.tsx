5e703024a6be90719f1ed34748ab4bee45ab4b4d
leonardosolar <leonardosolar@yahoo.com.br>
2023-05-22 11:17:31 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import TodoListView from './Presentation/Todo/TodoList/TodoListView';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TodoListView /> */}
+      <p>teste</p>
+    </div>
+  );
+}
+
+export default App;
